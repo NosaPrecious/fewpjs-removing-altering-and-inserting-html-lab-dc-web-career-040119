77a981b81c53a9da06ef1c3ca6c293bee224be1d
Nosa Okundaye <okundaye.preciousnosa@gmail.com>
2019-05-12 23:39:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Write your code here!
 document.querySelector('main#main').remove();
 const h1 = document.body.appendChild("h1");
-h1.setAttribute("id", "democlass");
+h1.setAttribute("id", "victory");
