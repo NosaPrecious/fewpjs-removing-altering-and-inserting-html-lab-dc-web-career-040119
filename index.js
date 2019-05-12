f3b199c6258c7a801695323e4fc62f6d305921cb
Nosa Okundaye <okundaye.preciousnosa@gmail.com>
2019-05-12 23:45:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,5 @@
 document.querySelector('main#main').remove();
 const newHeader = document.body.appendChild(document.createElement("h1"));
 newHeader.setAttribute("id", "victory");
+
+newHeader.innerHtml= "ken is the champion"
