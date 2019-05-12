4e78403706da3dfd0202ca8b754dfa56e447a7b9
Nosa Okundaye <okundaye.preciousnosa@gmail.com>
2019-05-12 23:43:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Write your code here!
 document.querySelector('main#main').remove();
-const newHeader = document.body.appendChild("h1");
-h1.setAttribute("id", "victory");
+const newHeader = document.body.appendChild(document.createElement("h1"));
+newHeader.setAttribute("id", "victory");
